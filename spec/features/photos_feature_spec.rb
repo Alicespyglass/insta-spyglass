66c465e0eac9e2e772302f0355e372abd747efd2
alicespyglass <alicespyglass@gmail.com>
2017-05-06 16:15:35 +0100

    Posts created and shown with titles only

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 feature 'photos' do
   context 'none have been added' do
-    scenario 'should display a prompt to add a photo' do
+    scenario 'should display a prompt to add a post' do
       visit '/photos'
-      expect(page).to have_content 'No photos yet'
-      expect(page).to have_link 'Add a photo'
+      expect(page).to have_content 'No posts yet'
+      expect(page).to have_link 'Add a post'
     end
   end
 
@@ -17,7 +17,19 @@ feature 'photos' do
     scenario 'display photos' do
         visit '/photos'
         expect(page).to have_content('DBC')
-        expect(page).not_to have_content('No photos yet')
+        expect(page).not_to have_content('No posts yet')
+    end
+  end
+
+  context 'creating posts' do
+    scenario 'prompts user to fill in form with title, then displays the new post' do
+      visit '/photos'
+      click_link 'Add a post'
+      fill_in 'Title', with: 'DBC'
+      click_button 'Create Photo'
+      expect(page).to have_content 'DBC'
+      expect(current_path).to eq '/photos'
+
     end
   end
 end
